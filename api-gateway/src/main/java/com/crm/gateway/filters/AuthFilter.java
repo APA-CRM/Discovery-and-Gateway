0de320fe3c86ca9cf438b43b4cb8c2deb30e879e
Pavel Tseva <paveltseva@gmail.com>
2026-02-24 21:47:44 +0200

    Update the authorization filters

--- api-gateway/src/main/java/com/crm/gateway/filters/AuthFilter.java
+++ api-gateway/src/main/java/com/crm/gateway/filters/AuthFilter.java
@@ -1,16 +1,22 @@
 package com.crm.gateway.filters;
 
+import com.crm.gateway.utils.JwtUtils;
+import com.crm.sharedlib.core.dto.request.AuthorizationRequest;
 import com.crm.sharedlib.core.dto.response.AuthResponse;
+import com.crm.sharedlib.core.exception.response.CrmErrorResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.util.Optional;
 
 import static com.crm.sharedlib.core.consts.CrmHeaders.USER_ID_HEADER_NAME;
 import static com.crm.sharedlib.core.consts.CrmHeaders.USER_LOGIN_HEADER_NAME;
@@ -33,10 +39,21 @@ public class AuthFilter extends BaseGatewayFilter {
         ServerHttpRequest request = exchange.getRequest();
 
         String authHeader = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+        String userAgent = request.getHeaders().getFirst(HttpHeaders.USER_AGENT);
+
+        Optional<String> tokenOptional = JwtUtils.getJwtTokenFromAuthorizationHeader(authHeader);
+
+        if (tokenOptional.isEmpty()) {
+            return respondWithError(exchange, 401, new CrmErrorResponse("Unauthorized"));
+        }
+
+        AuthorizationRequest authorizationRequest =
+                new AuthorizationRequest(tokenOptional.get(), userAgent);
 
         return webClient
-                .get()
+                .post()
                 .uri("/api/internal/auth/authorize")
+                .bodyValue(authorizationRequest)
                 .header(HttpHeaders.AUTHORIZATION, authHeader)
                 .retrieve()
                 .bodyToMono(AuthResponse.class)
