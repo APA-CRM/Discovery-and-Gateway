0a4b8409e9d692fa2e2695695926b33e91ed0b63
Pavel Tseva <paveltseva@gmail.com>
2026-03-15 11:05:32 +0200

    Small improvement  of AuthFilterTest

--- api-gateway/src/test/java/com/crm/gateway/filters/AuthFilterTest.java
+++ api-gateway/src/test/java/com/crm/gateway/filters/AuthFilterTest.java
@@ -3,14 +3,21 @@ package com.crm.gateway.filters;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 
+import static com.crm.sharedlib.core.consts.CrmHeaders.USER_ID_HEADER_NAME;
+import static com.crm.sharedlib.core.consts.CrmHeaders.USER_LOGIN_HEADER_NAME;
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
 class AuthFilterTest extends BaseGatewayFilterIntegrationTest {
+
+    @MockitoSpyBean
+    private AuthFilter filter;
 
     @Test
     @DisplayName("Should return 401 when Authorization header is missing")
@@ -25,15 +32,21 @@ class AuthFilterTest extends BaseGatewayFilterIntegrationTest {
                 .assertThat()
                 .statusCode(HttpStatus.UNAUTHORIZED.value())
                 .body("message", is("Unauthorized"));
+
+        Mockito.verify(filter, Mockito.atLeastOnce()).filter(Mockito.any(), Mockito.any());
     }
 
     @Test
     @DisplayName("Should forward request when valid JWT token")
     public void shouldForwardRequestWhenJwtValid() {
 
-        String accessToken = super.generateToken(1L, "login");
+        long userId = 1L;
+        String login = "login";
+        String accessToken = super.generateToken(userId, login);
 
         wireMock.stubFor(get(urlEqualTo("/api/organizations"))
+                .withHeader(USER_ID_HEADER_NAME, containing(String.valueOf(userId)))
+                .withHeader(USER_LOGIN_HEADER_NAME, containing(login))
                 .willReturn(aResponse()
                         .withStatus(200)
                         .withBody("OK")));
@@ -48,6 +61,7 @@ class AuthFilterTest extends BaseGatewayFilterIntegrationTest {
                 .assertThat()
                 .statusCode(HttpStatus.OK.value());
 
+        Mockito.verify(filter, Mockito.atLeastOnce()).filter(Mockito.any(), Mockito.any());
     }
 
 }
